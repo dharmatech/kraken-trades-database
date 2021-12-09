bb98650f4fc38ff16e03ab092c7666edc0dc8639
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-12-09 15:01:49 -0800

    updates

--- KrakenCandles/Program.cs
+++ KrakenCandles/Program.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace KrakenCandles // Note: actual namespace depends on the project name.
+namespace KrakenCandles
 {
     public class Candle
     {
@@ -24,23 +24,6 @@ namespace KrakenCandles // Note: actual namespace depends on the project name.
                 
                 var symbol_id = db.Symbols.First(symbol => symbol.Name == "XBTUSD").Id;
 
-                //var result =
-                //    db.Trades
-                //        .Where(trade => trade.SymbolId == symbol_id)
-                //        .GroupBy(trade => new { Year = trade.TimeStamp.Year, Month = trade.TimeStamp.Month })
-                //        .Select(group =>
-                //            new Candle()
-                //            {
-                //                DateTime = new DateTime(group.Key.Year, group.Key.Month, 1),
-                //                High = group.Max(trade => trade.Price),
-                //                Low = group.Min(trade => trade.Price)
-                //            });
-
-                // 00:00:11
-                // Fast! But does not have the Open and Close DateTime values.
-
-
-
                 var result =
                     db.Trades
                         .Where(trade => trade.SymbolId == symbol_id)
@@ -50,66 +33,11 @@ namespace KrakenCandles // Note: actual namespace depends on the project name.
                             {
                                 DateTime = new DateTime(group.Key.Year, group.Key.Month, 1),
                                 High = group.Max(trade => trade.Price),
-                                Low = group.Min(trade => trade.Price),
-                                Open = group.OrderBy(trade => trade.TimeStamp).First().Price,
-                                Close = group.OrderBy(trade => trade.TimeStamp).Last().Price
+                                Low = group.Min(trade => trade.Price)
                             });
 
-                // Unhandled exception.
-                // Npgsql.PostgresException (0x80004005): 42803:
-                // subquery uses ungrouped column "t.TimeStamp" from outer query
-
-                // Has Open and Close, however does not compile.
-
-
-                //var result =
-                //    db.Trades
-                //        .Where(trade => trade.SymbolId == symbol_id).ToList()
-                //        .GroupBy(trade => new { Year = trade.TimeStamp.Year, Month = trade.TimeStamp.Month })
-                //        .Select(group =>
-                //            new Candle()
-                //            {
-                //                DateTime = new DateTime(group.Key.Year, group.Key.Month, 1),
-                //                High = group.Max(trade => trade.Price),
-                //                Low = group.Min(trade => trade.Price),
-                //                Open = group.OrderBy(trade => trade.TimeStamp).First().Price,
-                //                Close = group.OrderBy(trade => trade.TimeStamp).Last().Price
-                //            });
-
-                // Explicit call to ToList()
-                // 6 minutes
-
-
-                //var result =
-                //    db.Trades
-                //        .Where(trade => trade.SymbolId == symbol_id).ToList()
-                //        .GroupBy(trade => new { Year = trade.TimeStamp.Year, Month = trade.TimeStamp.Month })
-                //        .Select(group =>
-                //            new Candle()
-                //            {
-                //                DateTime = new DateTime(group.Key.Year, group.Key.Month, 1),
-                //                High = group.Max(trade => trade.Price),
-                //                Low = group.Min(trade => trade.Price)
-                //            });
-
-                // Explicit call to ToList()
-                // 00:06:08
-
-
-
-
-
-                //var result =
-                //    db.Trades
-                //        .Where(trade => trade.SymbolId == symbol_id)
-                //        .GroupBy(trade => new { Year = trade.TimeStamp.Year, Month = trade.TimeStamp.Month })
-                //        .Select(group =>
-                //            new
-                //            {
-                //                DateTime = new DateTime(group.Key.Year, group.Key.Month, 1),
-                //                High = group.Max(trade => trade.Price),
-                //                Low = group.Min(trade => trade.Price)
-                //            });
+                // 00:00:11
+                // Fast! But does not have the Open and Close DateTime values.
 
                 foreach (var elt in result)
                 {
@@ -121,17 +49,7 @@ namespace KrakenCandles // Note: actual namespace depends on the project name.
                         elt.Close
                         );
                 }
-
-                //foreach (var elt in result)
-                //{
-                //    Console.WriteLine("{0:yyyy-MM} {1,10:F} {2,10:F} {3} {4}",
-                //        elt.DateTime,
-                //        elt.High,
-                //        elt.Low,
-                //        elt.open_time,
-                //        elt.close_time);
-                //}
-
+                                
                 timer.Stop();
 
                 Console.WriteLine();
